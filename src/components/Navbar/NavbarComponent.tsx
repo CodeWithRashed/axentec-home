880f4413d9b6f80cf6b1ffc7b8f6acc5cf9d6eb8
Md Rashed Ali <tools.rashed@gmail.com>
2024-04-15 10:07:06 +0600

    improved navbar

--- src/components/Navbar/NavbarComponent.tsx
+++ src/components/Navbar/NavbarComponent.tsx
@@ -1,10 +1,34 @@
-
 const NavbarComponent = () => {
   return (
-    <div style={{
-        background: "radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,0.25) 0%, rgba(0,0,0,1) 100%)"
-    }} className="w-full h-14 text-white">NavbarComponent</div>
-  )
-}
+    <div
+      style={{
+        background:
+          "radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,0.25) 0%, rgba(0,0,0,1) 100%)",
+      }}
+      className="w-full h-14 text-white flex items-center"
+    >
+      <div className="flex justify-between items-center w-full">
+        {/* MENU HEAD */}
+        <ul>
+          <li>Logo</li>
+        </ul>
 
-export default NavbarComponent
+        {/* MENU CENTER */}
+        <ul className="flex gap-3">
+          <li>Product & Solutions</li>
+          <li>Services</li>
+          <li>Industries</li>
+          <li>Career</li>
+          <li>About</li>
+        </ul>
+
+        {/* MENU END */}
+        <ul>
+          <li>Search</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarComponent;
