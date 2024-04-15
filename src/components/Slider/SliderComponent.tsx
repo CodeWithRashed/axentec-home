9f1c5c49678ec510adf9409cea14fced698ab9b7
Md Rashed Ali <tools.rashed@gmail.com>
2024-04-15 14:07:51 +0600

    improved slider

--- src/components/Slider/SliderComponent.tsx
+++ src/components/Slider/SliderComponent.tsx
@@ -1,40 +1,46 @@
-"use client"
+"use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, } from 'swiper/modules';
-
+import { Navigation, Pagination } from "swiper/modules";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import "./Swiper.css"
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./Swiper.css";
 
 export default function SliderComponent() {
-  const pagination = {
+  const PaginationOptions = {
     clickable: true,
-    
+    type: "bullets",
+    bulletClass: "swiper-custom-bullet",
+    bulletActiveClass: "swiper-custom-bullet-active",
   };
   return (
     <div>
-   <Swiper 
-      // install Swiper modules
-      modules={[Navigation, Pagination]}
-      spaceBetween={0}
-      slidesPerView={1.1}
-      centeredSlides={true}
-      loop={true}
-      navigation
-      pagination={pagination}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      className="h-screen w-screen"
-    >
-      <SwiperSlide className="h-[500px] bg-red-400">Slide 1</SwiperSlide>
-      <SwiperSlide className="h-[500px] bg-green-400">Slide 2</SwiperSlide>
-      <SwiperSlide className="h-[500px] bg-yellow-400">Slide 3</SwiperSlide>
-      <SwiperSlide className="h-[500px] bg-orange-400">Slide 4</SwiperSlide>
-    </Swiper>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination]}
+        spaceBetween={0}
+        slidesPerView={1.1}
+        centeredSlides={true}
+        loop={true}
+        navigation
+        pagination={{
+          // type: "bullets",
+          // bulletClass: "swiper-custom-bullet",
+          // bulletActiveClass: "swiper-custom-bullet-active",
+          clickable: true,
+        }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+        className="h-screen w-screen"
+      >
+        <SwiperSlide className="h-[500px] bg-red-400">Slide 1</SwiperSlide>
+        <SwiperSlide className="h-[500px] bg-green-400">Slide 2</SwiperSlide>
+        <SwiperSlide className="h-[500px] bg-yellow-400">Slide 3</SwiperSlide>
+        <SwiperSlide className="h-[500px] bg-orange-400">Slide 4</SwiperSlide>
+      </Swiper>
     </div>
   );
 }
